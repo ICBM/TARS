a1237b2e35c33805cee9e9256ecbe23008989ed5
unknown <Brett@.(none)>
2011-10-10 22:23:22 -0700

    Initial edit to make sure that everyone can view and edit the project solution.

---
+++ TARS/TARS/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TARS.Controllers
+{
+    public class TestController : Controller
+    {//NOTE: To access the controller, strip off 'Controller' from the class name.
+
+        //
+        // GET: /Test/
+        public ActionResult Index() //Right click here to add a View. View is now in Views/Test/___.cshtml
+        {
+            return View();
+        }
+
+        // 
+        // GET: /Test/Welcome/ 
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        //public string Welcome(int number){return "#"+number;} // - Does NOT like function overloading.
+
+        // GET: /Test/Parameters?name=""&number=3
+        public ActionResult Parameters(string name, int number = 5)
+        {
+            ViewBag.message = "Message: " + name;
+            ViewBag.number = number;
+            return View();
+        }
+    }
+}
