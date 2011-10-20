47e82f0889e293c5bfe980041d94e3adb1c14fd8
Brett <hitc8494@vandals.uidaho.edu>
2011-10-19 17:46:30 -0700

    Pushing some stubs for the User/Manager/Controller. Git gui didn't push correctly yesterday. >_>

---
+++ TARS/TARS/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TARS.Controllers
+{
+    public class AdminController : ManagerController
+    {
+        //
+        // GET: /Admin/
+        public override ActionResult Index()
+        {
+            return View();
+        }
+
+        public /*virtual*/ ActionResult addManager(string name)
+        {
+            ViewBag.name = name;
+            return View();
+        }
+    }
+}
---
+++ TARS/TARS/Controllers/ManagerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TARS.Controllers
+{
+    public class ManagerController : UserController
+    {
+        //
+        // GET: /Manager/
+        public override ActionResult Index() //Overridden from User/Index, which was virtual.
+        {
+            return View();
+        }
+
+        //
+        // GET: /Manager/addPCA
+        public virtual ActionResult addPCA(int code)
+        {
+            ViewBag.code = code;
+            return View();
+        }
+    }
+}
