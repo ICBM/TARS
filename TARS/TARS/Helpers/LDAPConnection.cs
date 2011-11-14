10a7827b0ab5727f55343c73f874981872fd1493
Scott Beddall <sbeddall@gmail.com>
2011-11-14 02:58:04 -0800

    Added Stubbed LDAP support. Still working to get interface working.
    Signed-off-by: Scott Beddall <sbeddall@gmail.com>

--- TARS/TARS/Helpers/LDAPConnection.cs
+++ TARS/TARS/Helpers/LDAPConnection.cs
@@ -2,18 +2,78 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.DirectoryServices;
+using System.DirectoryServices.Protocols;
+using System.Net;
 
 namespace TARS.Helpers
 {
     public class LDAPConnection
     {
+        private int exists = 0; //if the user exists, this will be 1. ensures that requestUser() is called before requestRole()  
+        private static string user = "admin"; //we'll re-use these within the class. make it easily modifiable for the users who come after us
+        private static string pw = "password";
+        private static string domain = "localhost"; //built for local server
+        private static string port = ":10389"; //default port for ActiveDirectory LDAP is 389. ApacheDS uses 10389
+
+        private NetworkCredential credential; //one credential to go with the one connection; built using user + pw + domain
+        private LdapDirectoryIdentifier identifier; //built using domain + port
+        private LdapConnection connection; //one connection to each LDAPConnection class instance; built using identifier
+
+        private string dn;
+
+
         public LDAPConnection()
         {
+            connection = new LdapConnection(domain);
+            credential = new NetworkCredential(user, pw, domain);
+            identifier = new LdapDirectoryIdentifier(domain + port);
 
         }
-        public bool establishConnection()
+
+        //if you really want to, go ahead and specify your specific information
+        public LDAPConnection(string specifiedUser, string specifiedPw, string specifiedDomain, string specifiedPort)
         {
-            return true;
+            user = specifiedUser;
+            pw = specifiedPw;
+            domain = specifiedDomain;
+            port = specifiedPort;
+
+            connection = new LdapConnection(domain);
+            credential = new NetworkCredential(user, pw, domain);
+            identifier = new LdapDirectoryIdentifier(domain + port);
+
         }
+
+        //if this returns true, go ahead and add the model.user to the cookie for login
+        public bool requestUser(string user, string password)
+        {
+
+
+            return false;
+        }
+
+        //return "User", "Manager", or "Admin" depending on the role
+        public string requestRole(string user)
+        {
+            if (exists == 1)
+            {
+                return "User";
+            }
+
+            else return "None";
+        }
+
+
+        public string[] requestTasks(string user)
+        {
+
+            return null;
+        }
+
+
+
+
+
     }
 }
