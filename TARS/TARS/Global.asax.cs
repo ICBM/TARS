1af77332dca03f9d7be42df59cdb08d547740705
Zeke Long <zeke_long@hotmail.com>
2012-05-02 21:09:07 -0700

    got Quartz.NET scheduler working (still need to trigger specific events though)

--- TARS/TARS/Global.asax.cs
+++ TARS/TARS/Global.asax.cs
@@ -5,7 +5,14 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace TARS
+using TARS.Controllers;
+using TARS.Models;
+
+using Quartz;   //for scheduling jobs
+using Quartz.Impl;
+using Quartz.Impl.Triggers;
+
+namespace TARS 
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
@@ -17,6 +24,7 @@ namespace TARS
             filters.Add(new HandleErrorAttribute());
         }
 
+        //
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
@@ -34,15 +42,65 @@ namespace TARS
 				new { controller = "Home", action = "Index", id = UrlParameter.Optional } //Parameter defaults
 */
             );
-
         }
 
+
+        //
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
+
+            // start the job scheduler
+            ConfigureQuartzJobs(); 
         }
+
+
+        //
+        // Uses Quartz.NET package to schedule jobs
+        public static void ConfigureQuartzJobs()
+        {
+	        // construct a scheduler factory
+	        ISchedulerFactory schedFact = new StdSchedulerFactory();
+	 
+	        // get a scheduler
+	        IScheduler sched = schedFact.GetScheduler();
+	        sched.Start();
+
+            /* Trigger to lock timesheets every Tuesday and Wednesday morning at 12:00am.
+             * Timesheets won't be locked on Tuesday if there was a holiday, and won't be
+             * locked on Wednesday if there wasn't a holiday
+             */
+            JobDetailImpl job1 = new JobDetailImpl("lockTimesheets", typeof(TARS.ScheduledJobs.TarsScheduledJobs));
+            CronTriggerImpl lockTimesheets = new CronTriggerImpl("lock",
+                                                                  null,
+                                                                  "lockTimesheets",
+                                                                  null,
+                                                                  "0 0 0 ? * 'TUE,WED'"
+                                                                );
+            sched.ScheduleJob(job1, lockTimesheets);
+
+            // trigger to send reminder email to employees to submit timesheets by end of day Saturday
+            JobDetailImpl job2 = new JobDetailImpl("submitTsRemind", typeof(TARS.ScheduledJobs.TarsScheduledJobs));
+            CronTriggerImpl submitTimesheetReminder = new CronTriggerImpl("submitReminder",
+                                                                           null,
+                                                                           "submitTsRemind",
+                                                                           null,
+                                                                           "0 0 0 ? * SAT"
+                                                                         );
+            sched.ScheduleJob(job2, submitTimesheetReminder);
+
+            // trigger to send reminder email to managers and admin when a PCA or Work Effort expire that week
+            JobDetailImpl job3 = new JobDetailImpl("pcaAndWeExpRemind", typeof(TARS.ScheduledJobs.TarsScheduledJobs)); 
+            CronTriggerImpl pcaAndWeExpirationReminder = new CronTriggerImpl("pcaAndWeReminder",
+                                                                              null,
+                                                                              "pcaAndWeExpRemind",
+                                                                              null,
+                                                                              "0 0 0 ? * SUN"
+                                                                            );
+            sched.ScheduleJob(job3, pcaAndWeExpirationReminder);
+	    }
     }
 }
---
+++ TARS/TARS/ScheduledJobs/TarsScheduledJobs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+
+using Quartz; 
+
+
+namespace TARS.ScheduledJobs
+{
+    public class TarsScheduledJobs : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+	    {
+            if (true)
+            {
+                Debug.WriteLine("Hello at " + DateTime.Now.ToString() + " --- " + context);
+            }
+	    }
+    }
+}
